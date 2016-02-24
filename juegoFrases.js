55fc15e74f357d8fd637f532a848e6b515e2aaed
milincheta <milincheta@hotmail.com>
2016-02-24 14:13:24 -0300

    version online

--- juegoFrases.js
+++ juegoFrases.js
@@ -1,39 +1,57 @@
 Frases = new Mongo.Collection('frases');
 
 
-
 if (Meteor.isClient) {
 
+Accounts.config({
+  forbidClientAccountCreation: true,
+});
+
+Accounts.ui.config({
+     passwordSignupFields: "USERNAME_ONLY"
+   });
+
+
   // This code only runs on the client
-  angular.module('JuegoFrases',['angular-meteor']);
+  angular.module('JuegoFrases',['angular-meteor', 'accounts.ui']);
 
-  angular.module('JuegoFrases').controller('eliminarFrase', ['$scope', '$meteor',
-      function ($scope, $meteor) {
-
-        $scope.eliminarFrase = function ($scope) {
-          $scope.Frases.remove({frase:"frase"});
-                  };
-
-  }]);
-
-
+//controller listaFrases
   angular.module('JuegoFrases').controller('listaFrases', ['$scope', '$meteor',
       function ($scope, $meteor) {
 
+
+        // funciona para retornar las frases ordenadas, adicionalmente
+        //relaciona el modelo en el html con el modelo de mongo db :  ng-repeat="frase in frases"
         $scope.frases = $meteor.collection( function() {
-              return Frases.find({}, { sort: { frase: 1 } })
-            });
-}]);
+        return Frases.find({}, { sort: { frase: 1 } })
+                                                        }
+      );
 
-  angular.module('JuegoFrases').controller('agregarFrase', ['$scope', '$meteor',
-    function ($scope, $meteor) {
+          //funcion para agregar una frase
+      $scope.agregarFrase =  function(nueva) {
+                              $scope.frases.push( {
+                              frase: nueva.frase,
+                              palabraClave: nueva.palabraClave,
+                              significadoCorrecto: nueva.significadoCorrecto,
+                              significadoIncorrecto: nueva.significadoIncorrecto,
+                              principiante: nueva.principiante,
+                              intermedio: nueva.intermedio,
+                              avanzado: nueva.avanzado,
+                              createdAt: new Date() })
+                            };
 
-      $scope.agregarFrase = function ($scope) {
-        $scope.Frases.push( {
-          frase: frase,
-          createdAt: new Date() }
-        );
-      };
+        //funcion para eliminar una frase ya cargada
+        $scope.eliminarFrase = function (id) {
+          if (confirm("Borrar?")) {
+              $scope.frases.remove({_id:id});
+            }
+        };
 
-    }]);
-}
+
+
+
+
+        }]);
+//fin del codigo del controller listaFrases
+
+};
